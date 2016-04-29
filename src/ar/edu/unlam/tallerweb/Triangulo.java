3fd5d7d37a7d17bf81cb08b12ab85658ba1319b8
AldanaDiFlavio <aldanadiflavio@live.com.ar>
2016-04-29 16:50:08 -0300

    Added files via upload

---
+++ src/ar/edu/unlam/tallerweb/Triangulo.java
@@ -0,0 +1,15 @@
+package ar.edu.unlam.tallerweb;
+
+public class Triangulo {
+	Double resultado;
+	
+	public Double perimetro(Double lado) {
+		return resultado = 3*lado;
+	}
+
+	public Double area(Double base, Double altura) {
+		// TODO Auto-generated method stub
+		return resultado = (base*altura)/2;
+	}
+
+}
